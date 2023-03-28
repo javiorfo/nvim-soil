7c94df50ed9bcd2c7e20b82bfb4ebe8a4b4b0dd5
Javier Orfo <javierorfo@protonmail.com>
2023-03-28 00:24:19 -0300

    Update plantuml.lua

--- ftdetect/plantuml.lua
+++ ftdetect/plantuml.lua
@@ -1,6 +1,6 @@
--- #######################################################
--- # Maintainer: System Malt                             #
--- # URL:        https://github.com/systemmalt/nvim-soil #
--- #######################################################
+-- #####################################################
+-- # Maintainer: Javier Orfo                           #
+-- # URL:        https://github.com/javiorfo/nvim-soil #
+-- #####################################################
 
 vim.cmd[[autocmd BufRead,BufNewFile *.uml,*.pu,*.plantuml,*.puml set filetype=plantuml]]
