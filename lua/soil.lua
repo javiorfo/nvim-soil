3902c1f238c8433c369842e2bef803b4a3e0a895
Javier Orfo <javierorfo@protonmail.com>
2023-03-28 00:24:27 -0300

    Update soil.lua

--- lua/soil.lua
+++ lua/soil.lua
@@ -1,7 +1,7 @@
--- #######################################################
--- # Maintainer: System Malt                             #
--- # URL:        https://github.com/systemmalt/nvim-soil #
--- #######################################################
+-- #####################################################
+-- # Maintainer: Javier Orfo                           #
+-- # URL:        https://github.com/javiorfo/nvim-soil #
+-- #####################################################
 
 local Logger = require'soil.logger':new("Soil")
 local M = {}
