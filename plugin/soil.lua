ffb7f720cee7d890f263238119fd2858a6b4900b
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:23:06 -0300

    Update soil.lua

--- plugin/soil.lua
+++ plugin/soil.lua
@@ -1,8 +1,3 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-soil #
--- ######################################################
-
 if vim.g.soil then
   return
 end
