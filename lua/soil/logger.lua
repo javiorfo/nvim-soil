5508abf99a8acacfaae7e0da07bb184659bbd6bc
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:22:55 -0300

    Update logger.lua

--- lua/soil/logger.lua
+++ lua/soil/logger.lua
@@ -1,8 +1,3 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-soil #
--- ######################################################
-
 local M = {}
 
 local function logger(plugin_name, msg)
